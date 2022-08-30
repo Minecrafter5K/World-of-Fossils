4ed5a113ee64af03701b74fafb0ff42d45237706
Minecrafter5K <tristan.jockel@icloud.com>
2022-08-30 21:56:37 +0200

    🚀 added component to view objects

--- DatenbankWeb/src/app/app.module.ts
+++ DatenbankWeb/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CubeComponent } from './cube/cube.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CubeComponent
   ],
   imports: [
     BrowserModule,
